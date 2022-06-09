8498784d83b17b895e8ff7dd25cb6051687df1fc
azizjarrar <tripelbil@gmail.com>
2022-06-09 14:52:45 +0200

    remake commit

--- routes/platform.js
+++ routes/platform.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const {upload} = require('../global_functions/multer')
 
 const platfromController = require('../controllers/platform.controller')
 
-router.post('/addPlatform', platfromController.addPlatform)
-router.put('/updatePlatform', platfromController.updatePlatform)
+router.post('/addPlatform',upload('platformIconForProdcustAndGiftCards').single('icon'), platfromController.addPlatform)
+router.put('/updatePlatform',upload('platformIconForProdcustAndGiftCards').single('icon'), platfromController.updatePlatform)
 router.delete('/deletePlatform', platfromController.deletePlatform)
 router.get('/getAllPlatform', platfromController.getAllPlatform)
 module.exports = router
